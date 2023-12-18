cf50a4bb30d7654b3d26a387c04c4664aea8b2ac
User <User@HOME>
2023-12-18 12:41:00 +0700

    Добавьте файлы проекта.

---
+++ MyForm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace PasswordGenerator
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.upCaseCheckBox = new System.Windows.Forms.CheckBox();
+            this.lowCaseCheckBox = new System.Windows.Forms.CheckBox();
+            this.numberCheckBox = new System.Windows.Forms.CheckBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.lengthComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // upCaseCheckBox
+            // 
+            this.upCaseCheckBox.AutoSize = true;
+            this.upCaseCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.upCaseCheckBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.upCaseCheckBox.Location = new System.Drawing.Point(12, 119);
+            this.upCaseCheckBox.Name = "upCaseCheckBox";
+            this.upCaseCheckBox.Size = new System.Drawing.Size(230, 33);
+            this.upCaseCheckBox.TabIndex = 0;
+            this.upCaseCheckBox.Text = "Верхний регистр";
+            this.upCaseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // lowCaseCheckBox
+            // 
+            this.lowCaseCheckBox.AutoSize = true;
+            this.lowCaseCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lowCaseCheckBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lowCaseCheckBox.Location = new System.Drawing.Point(12, 169);
+            this.lowCaseCheckBox.Name = "lowCaseCheckBox";
+            this.lowCaseCheckBox.Size = new System.Drawing.Size(225, 33);
+            this.lowCaseCheckBox.TabIndex = 1;
+            this.lowCaseCheckBox.Text = "Нижний регистр";
+            this.lowCaseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // numberCheckBox
+            // 
+            this.numberCheckBox.AutoSize = true;
+            this.numberCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numberCheckBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.numberCheckBox.Location = new System.Drawing.Point(12, 218);
+            this.numberCheckBox.Name = "numberCheckBox";
+            this.numberCheckBox.Size = new System.Drawing.Size(113, 33);
+            this.numberCheckBox.TabIndex = 2;
+            this.numberCheckBox.Text = "Цифры";
+            this.numberCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // generateButton
+            // 
+            this.generateButton.BackColor = System.Drawing.Color.Gray;
+            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generateButton.ForeColor = System.Drawing.Color.Lime;
+            this.generateButton.Location = new System.Drawing.Point(12, 274);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(225, 50);
+            this.generateButton.TabIndex = 3;
+            this.generateButton.Text = "Сгенерировать";
+            this.generateButton.UseVisualStyleBackColor = false;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.Gray;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.ForeColor = System.Drawing.Color.Red;
+            this.exitButton.Location = new System.Drawing.Point(284, 274);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(62, 50);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Х";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Пароль:";
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.ForeColor = System.Drawing.Color.Black;
+            this.textBox.Location = new System.Drawing.Point(123, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(230, 35);
+            this.textBox.TabIndex = 6;
+            // 
+            // lengthComboBox
+            // 
+            this.lengthComboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lengthComboBox.DisplayMember = "6";
+            this.lengthComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lengthComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lengthComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lengthComboBox.FormattingEnabled = true;
+            this.lengthComboBox.Items.AddRange(new object[] {
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.lengthComboBox.Location = new System.Drawing.Point(202, 65);
+            this.lengthComboBox.MaxDropDownItems = 10;
+            this.lengthComboBox.Name = "lengthComboBox";
+            this.lengthComboBox.Size = new System.Drawing.Size(121, 37);
+            this.lengthComboBox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Длина пароля:";
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(365, 336);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lengthComboBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.numberCheckBox);
+            this.Controls.Add(this.lowCaseCheckBox);
+            this.Controls.Add(this.upCaseCheckBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MyForm";
+            this.Text = "Генератор пароля";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox upCaseCheckBox;
+        private System.Windows.Forms.CheckBox lowCaseCheckBox;
+        private System.Windows.Forms.CheckBox numberCheckBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ComboBox lengthComboBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MyForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PasswordGenerator
+{
+    public partial class MyForm : Form
+    {
+        public string[] upCase = { "QWERTYUIOPASDFGHJKLZXCVBNM" };
+        public string[] lowCase = { "qwertyuiopasdfghjklzxcvbnm" };
+        public string[] numberCase = { "1234567890" };
+        public MyForm()
+        {
+            InitializeComponent();
+            lengthComboBox.Text = lengthComboBox.Items[2].ToString();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Вы действительно хотите выйти?",
+                "Выход",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            int size = Convert.ToInt32(lengthComboBox.SelectedItem);
+            StringBuilder sb = new StringBuilder();
+            if (upCaseCheckBox.Checked)
+            {
+                for (int i = 0; i < upCase.Length; i++)
+                {
+                    sb.Append(upCase[i]);
+                }
+            }
+            if (lowCaseCheckBox.Checked)
+            {
+                for (int i = 0; i < lowCase.Length; i++)
+                {
+                    sb.Append(lowCase[i]);
+                }
+            }
+            if (numberCheckBox.Checked)
+            {
+                for (int i = 0; i < numberCase.Length; i++)
+                {
+                    sb.Append(numberCase[i]);
+                }
+            }
+            if (!upCaseCheckBox.Checked && !lowCaseCheckBox.Checked && !numberCheckBox.Checked)
+            {
+                DialogResult result = MessageBox.Show(
+                    "Необходимо выбрать какие символы будут использованны в пароле",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                return;
+            }
+            string str = "";
+            Random r = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                str += sb[r.Next(0, sb.Length)];
+            }
+            textBox.Text = str;
+        }
+    }
+}
